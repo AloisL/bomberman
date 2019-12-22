ca5dd975af0e221d83b0cfc03d64d59938431b39
CChaudeurdy <55496402+CChaudeurdy@users.noreply.github.com>
2019-12-22 23:07:07 +0100

    Update BombermanAgent.java

--- bomberman/src/main/java/bomberman/model/agent/BombermanAgent.java
+++ bomberman/src/main/java/bomberman/model/agent/BombermanAgent.java
@@ -5,14 +5,17 @@ import bomberman.model.engine.enums.AgentAction;
 import bomberman.model.engine.enums.ColorAgent;
 import bomberman.model.engine.enums.StateBomb;
 import bomberman.model.engine.info.InfoBomb;
-import bomberman.model.strategie.StrategieBasic;
-
+import bomberman.model.strategie.StrategieAttaque;
+import bomberman.model.strategie.StrategieSafe;
 
 public class BombermanAgent extends AbstractAgent {
 
     private int nbMaxBomb;
     private int nbBombPlaced;
     private int bombRange;
+    private int nbLifes = 3;
+    private int nbTurnInvinsible;
+    private int nbTurnSick;
 
     public BombermanAgent(int x, int y, AgentAction agentAction, ColorAgent color, boolean isInvincible,
                           boolean isSick) {
@@ -20,42 +23,48 @@ public class BombermanAgent extends AbstractAgent {
         nbMaxBomb = 1;
         bombRange = 1;
         nbBombPlaced = 0;
-        rangeView= 5;
-
-
+        rangeView = 5;
     }
 
-    public void setStrategie(BombermanGame bombermanGame) {
-
-       /* StrategieSafe strategieSafe=new StrategieSafe(bombermanGame,this);
-        setStrategieAgents(strategieSafe);
-
-
-        if(strategieSafe.checkSiBesoinSafe()==null) {
-            StrategieAttaque attaque = new StrategieAttaque(bombermanGame, this);
-            setStrategieAgents(attaque);
-        }
-        */
-        StrategieBasic strat = new StrategieBasic(bombermanGame,this);
-        setStrategieAgents(strat);
-
-
-    }
-
-        public boolean canPlaceBomb() {
-        if (nbBombPlaced < nbMaxBomb) return true;
+    public boolean canPlaceBomb() {
+        if (!isSick() && (nbBombPlaced < nbMaxBomb)) return true;
         else return false;
     }
+
+    @Override
+    public void setStrategie(BombermanGame bombermanGame) {
+        /*
+        StrategieAttaque attaque = new StrategieAttaque(bombermanGame,this);
+        setStrategieAgents(attaque);
+
+        if (this.getStrategie().checkSiBesoinSafe()!=null){
+            StrategieSafe safeStrat=new StrategieSafe(bombermanGame,this);
+            setStrategieAgents(safeStrat);
+        }else {
+            this.setStrategieAgents(attaque);
+        }
+
+         */
 /*
-    public boolean isSafeSibombe(BombermanGame bombermanGame){
-        StrategieSafe strat=new StrategieSafe(bombermanGame,this);
-        InfoBomb bomb=new InfoBomb(this,this.getX(),this.getY(),this.getBombRange(),StateBomb.Step1);
-        System.out.println(strat.zoneSafe(bomb).x+" : "+strat.zoneSafe(bomb).y);
-        if (strat.zoneSafe(bomb).x==0) return false;
-        return true;
-    }
+        StrategieSafe strategieSafe=new StrategieSafe(bombermanGame,this);
+        setStrategieAgents(strategieSafe);
 
  */
+        //    if(strategieSafe.checkSiBesoinSafe()==null) {
+        StrategieAttaque attaque = new StrategieAttaque(bombermanGame, this);
+        setStrategieAgents(attaque);
+        //   }
+
+
+    }
+
+    public boolean isSafeSibombe(BombermanGame bombermanGame) {
+        StrategieSafe strat = new StrategieSafe(bombermanGame, this);
+        InfoBomb bomb = new InfoBomb(this, getX(), getY(), getBombRange(), StateBomb.Step1);
+        System.out.println(strat.zoneSafe(bomb).x + " : " + strat.zoneSafe(bomb).y);
+        if (strat.zoneSafe(bomb).x == 0) return false;
+        return true;
+    }
 
     public InfoBomb addBomb() {
         InfoBomb bomb = new InfoBomb(this, getX(), getY(), bombRange, StateBomb.Step1);
@@ -67,17 +76,58 @@ public class BombermanAgent extends AbstractAgent {
         nbBombPlaced--;
     }
 
-    public void setNbMaxBomb(int nbMaxBomb) {
-        this.nbMaxBomb = nbMaxBomb;
+    public void removeLife() {
+        nbLifes--;
     }
 
-    public void setBombRange(int bombRange) {
-        this.bombRange = bombRange;
+    /**
+     * Retourne le statut de mort l'agent (true == mort, false == vivant)
+     *
+     * @return boolean
+     */
+    public boolean isDead() {
+        if (nbLifes < 1) return true;
+        else return false;
     }
 
-    public int getBombRange(){
-        return this.bombRange;
+    public void addMaxBomb() {
+        nbMaxBomb++;
     }
 
+    public void reduceMaxBomb() {
+        if (nbMaxBomb > 1) nbMaxBomb--;
+    }
+
+    public void addBombRange() {
+        bombRange++;
+    }
+
+    public void reduceBombRange() {
+        if (bombRange > 1) bombRange--;
+    }
+
+    public void makeInvinsible() {
+        setInvincible(true);
+        nbTurnInvinsible = 20;
+    }
+
+    public void reduceInvinsibleTurn() {
+        nbTurnInvinsible--;
+        if (nbTurnInvinsible <= 0) setInvincible(false);
+    }
+
+    public void makeSick() {
+        setSick(true);
+        nbTurnSick = 10;
+    }
+
+    public void reduceSickTurn() {
+        nbTurnSick--;
+        if (nbTurnSick <= 0) setSick(false);
+    }
+
+    public int getBombRange() {
+        return bombRange;
+    }
 
 }
