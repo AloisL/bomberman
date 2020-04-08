7dd65329e21c74209b35e630525dd16ac8478775
CChaudeurdy <chaudeurdy.corentin@hotmail.fr>
2020-04-08 23:59:34 +0200

    je sais plus

--- BombermanWeb/src/main/java/ua/info/m1/bomberman/controller/LeaderboardController.java
+++ BombermanWeb/src/main/java/ua/info/m1/bomberman/controller/LeaderboardController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.WebUtils;
 
+import ua.info.m1.bomberman.model.entities.NbVictoirDef;
 import ua.info.m1.bomberman.model.entities.User;
-
+import ua.info.m1.bomberman.model.repositories.HistoriqueGameRepository;
 import ua.info.m1.bomberman.model.repositories.UserRepository;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,14 +25,26 @@ public class LeaderboardController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private HistoriqueGameRepository historiqueGameRepository;
 
 
     @GetMapping("/bomberman/leaderboard")
-    public String leaderboard(HttpServletRequest request) {
+    public String leaderboard(HttpServletRequest request,Model model) {
         Cookie cookieToken = WebUtils.getCookie(request, "session");
         String token = cookieToken.getValue();
         User user = userRepository.findUserByCurrentToken(token);
-        if (user != null) return "leaderboard";
+        
+        
+        if (user != null) { 
+        	ArrayList<NbVictoirDef> lader= new ArrayList<>();
+            for (User u : userRepository.findAll()) {
+            	NbVictoirDef userVictoirDef = new NbVictoirDef(historiqueGameRepository.findAll(), u.getUsername(), u.getId());
+            	lader.add(userVictoirDef);
+    		}
+            lader.add(new NbVictoirDef());
+            model.addAttribute("lader",lader);
+        	return "leaderboard";}
         else return "connect";
     }
 
--- BombermanWeb/src/main/java/ua/info/m1/bomberman/model/repositories/UserRepository.java
+++ BombermanWeb/src/main/java/ua/info/m1/bomberman/model/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package ua.info.m1.bomberman.model.repositories;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import ua.info.m1.bomberman.model.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+	
+	List<User> findAll();
 
     User findByUsername(String login);
 
